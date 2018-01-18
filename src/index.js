83f923b0f5480755467ca4998d191801b5667e1f
Aniruddh Gopale <anigopale@gmail.com>
2018-01-18 22:03:56 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import 'semantic-ui-css/semantic.min.css';
+
+import './index.css';
+import App from './App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
