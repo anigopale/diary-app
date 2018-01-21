26e8e71e7d82bf741d761dc913335dba7a709e20
Aniruddh Gopale <anigopale@gmail.com>
2018-01-21 22:53:49 +0530

    update user-reducer

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,7 +1,6 @@
 import { USER, LOGOUT } from '../actions/types';
 
 export default function(state="", action) {
-  console.log("action reached:", action.payload);
   switch (action.type) {
     case USER:
       return action.payload;
