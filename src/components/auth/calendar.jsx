af647dbfd481a9e30f0b80eee97d7e35cd6afe33
Aniruddh Gopale <anigopale@gmail.com>
2018-01-24 01:42:40 +0530

    make calendar component interactive/dynamic

--- src/components/auth/calendar.jsx
+++ src/components/auth/calendar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Header } from 'semantic-ui-react';
+import { Grid, Segment, Header, Icon } from 'semantic-ui-react';
 import calendar from 'calendar-js';
 
 export default class Calendar extends Component {
@@ -7,7 +7,8 @@ export default class Calendar extends Component {
     super(props);
     this.state = {
       cal: calendar().of(this.props.year, this.props.month).calendar,
-      month: calendar().of(this.props.year, this.props.month).month,
+      monthStr: calendar().of(this.props.year, this.props.month).month,
+      month: this.props.month,
       year: this.props.year
     }
   }
@@ -18,13 +19,13 @@ export default class Calendar extends Component {
   renderWeek(week) {
     return week.map((d) => {
       if(d === 0) return <Grid.Column></Grid.Column>
-      if(d === 16) return <Grid.Column><Header as="h2" color="green">{d}</Header></Grid.Column>
-      return <Grid.Column onClick={()=>this.setState({selected: d})}><Header as='h2' color="grey">{d}</Header></Grid.Column>
+      if(d === this.props.day) return <Grid.Column><Header color="blue">{d}</Header></Grid.Column>
+      return <Grid.Column onClick={()=>this.setState({selected: d})}>{d}</Grid.Column>
     })
   }
 
   renderMonth() {
-    return this.cal.map((week) => {
+    return this.state.cal.map((week) => {
       return (
         <div>
           <Grid horizontal >
@@ -36,15 +37,79 @@ export default class Calendar extends Component {
     })
   }
 
+  upYear() {
+    this.setState({
+      cal: calendar().of(this.state.year + 1, this.state.month).calendar,
+      monthStr: calendar().of(this.state.year + 1, this.state.month).month,
+      month: this.state.month,
+      year: this.state.year + 1
+    });
+  }
+  downYear() {
+    this.setState({
+      cal: calendar().of(this.state.year - 1, this.state.month).calendar,
+      monthStr: calendar().of(this.state.year - 1, this.state.month).month,
+      month: this.state.month,
+      year: this.state.year - 1
+    });
+  }
+  upMonth() {
+    if(this.state.month === 11) {
+      this.upYear();
+      this.setState({
+        cal: calendar().of(this.state.year + 1, 0).calendar,
+        monthStr: calendar().of(this.state.year + 1, 0).month,
+        month: 0,
+        year: this.state.year + 1
+      });
+    }
+    else {
+      this.setState({
+        cal: calendar().of(this.state.year, this.state.month + 1).calendar,
+        monthStr: calendar().of(this.state.year, this.state.month + 1).month,
+        month: this.state.month + 1,
+        year: this.state.year
+      });
+    }
+  }
+  downMonth() {
+    if(this.state.month === 0) {
+      this.downYear();
+      this.setState({
+        cal: calendar().of(this.state.year - 1, 11).calendar,
+        monthStr: calendar().of(this.state.year - 1, 11).month,
+        month: 11,
+        year: this.state.year - 1
+      });
+    }
+    else {
+      this.setState({
+        cal: calendar().of(this.state.year, this.state.month - 1).calendar,
+        monthStr: calendar().of(this.state.year, this.state.month - 1).month,
+        month: this.state.month - 1,
+        year: this.state.year
+      });
+    }
+  }
+
   render() {
     console.log(calendar().of(2018, 2));
     console.log(this.props.year);
     return(
       <div>
         <Segment>
-          <Header as="h2">
-            {`${this.state.month} ${this.state.year}`}
-          </Header>
+          <h2>
+            <Icon name="chevron left" onClick={this.downYear.bind(this)} />
+            {this.state.year}
+            <Icon name="chevron right" onClick={this.upYear.bind(this)} />
+          </h2>
+
+          <h2>
+            <Icon name="chevron left" onClick={this.downMonth.bind(this)} />
+            {this.state.monthStr}
+            <Icon name="chevron right" onClick={this.upMonth.bind(this)} />
+          </h2>
+
           {this.renderMonth()}
         </Segment>
       </div>
