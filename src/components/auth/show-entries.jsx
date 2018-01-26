e6eaeb78680cbb98766f8ac4e13122e3031acb1e
Aniruddh Gopale <anigopale@gmail.com>
2018-01-26 23:37:31 +0530

    add markdown support

--- src/components/auth/show-entries.jsx
+++ src/components/auth/show-entries.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Segment, Container, Button } from 'semantic-ui-react';
+import marked from 'marked';
 import { fetchData, showSelectedEntry } from '../../actions';
 
 class ShowEntries extends Component {
@@ -13,12 +14,23 @@ class ShowEntries extends Component {
     this.props.fetchData();
   }
 
+  markUp(note) {
+
+    let text = marked(note, {sanitize: true});
+    return {
+      __html: text
+    };
+  }
+
   showEntry() {
     return (
-      <Container>
+      <Container text>
         <Button onClick={() => {this.setState({ selected: false })}}>Back</Button>
         <h2>{this.props.selected_data.date}</h2>
-        <p>{this.props.selected_data.note}</p>
+        <p
+          dangerouslySetInnerHTML={this.markUp(this.props.selected_data.note)}
+          />
+
       </Container>
     )
   }
