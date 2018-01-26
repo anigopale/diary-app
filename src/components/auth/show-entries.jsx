b55a2070029307ea7a3c1786c7061c204b1a2ef2
Aniruddh Gopale <anigopale@gmail.com>
2018-01-26 22:36:28 +0530

    render fetched data

--- src/components/auth/show-entries.jsx
+++ src/components/auth/show-entries.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Segment } from 'semantic-ui-react';
 import { fetchData } from '../../actions';
 
 class ShowEntries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: false }
+  }
 
   componentDidMount() {
     this.props.fetchData();
   }
 
+  renderEntries() {
+    if(!this.props.data) {
+      return <h1>No entries found</h1>
+    }
+    return this.props.data.map((data) => {
+      return (
+        <Segment>
+          {data.date}
+        </Segment>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        
+        {this.renderEntries()}
       </div>
     )
   }
 }
 
+function mapStateToProps({ data }) {
+  return { data };
+}
 
-
-export default connect(null, { fetchData })(ShowEntries);
+export default connect(mapStateToProps, { fetchData })(ShowEntries);
