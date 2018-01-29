82637b2c76b9ad2521b3ad2feae6bcdaa2415a7e
Aniruddh Gopale <anigopale@gmail.com>
2018-01-29 23:53:03 +0530

    create exportCal function

--- src/components/auth/export-cal.jsx
+++ src/components/auth/export-cal.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
 import { Button } from 'semantic-ui-react';
+import { fetchData } from '../../actions';
 
-export default class ExportCal extends Component {
+class ExportCal extends Component {
+  componentDidMount() {
+    this.props.fetchData();
+  }
+
+  exportCal() {
+    let ical_begin = `BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//DiaryApp//\n`
+    let ical_end = `END:VCALENDAR`;
+    let ical_events = '';
+    this.props.data.map((d) => {
+      let date = moment(d.time, 'x').format('YYYYMMDD');
+      let time = moment(d.time, 'x').format('HHmmss');
+      let dtstamp = `${date}T${time}Z`;
+      ical_events = ical_events + `BEGIN:VEVENT\nUID:${localStorage.getItem('user')}\nDTSTAMP:${dtstamp}\nSUMMARY:${d.note}\nEND:VEVENT\n`;
+    });
+    console.log(`${ical_begin}${ical_events}${ical_end}`);
+  }
+
   render() {
     return (
       <div>
@@ -10,7 +30,7 @@ export default class ExportCal extends Component {
           Export your Diary data
         </p>
         <Button
-          onClick={() => {this.props.deleteAccount()}}
+          onClick={this.exportCal.bind(this)}
           primary
           >
           Export
@@ -19,3 +39,9 @@ export default class ExportCal extends Component {
     )
   }
 }
+
+function mapStateToProps({ data }) {
+  return { data };
+}
+
+export default connect(mapStateToProps, { fetchData })(ExportCal);
