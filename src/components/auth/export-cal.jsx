e1ccbe56d52b8d198ccf347190ec53e690c8c66a
Aniruddh Gopale <anigopale@gmail.com>
2018-01-29 22:31:23 +0530

    add export calendar UI, minor changes

--- src/components/auth/export-cal.jsx
+++ src/components/auth/export-cal.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 export default class ExportCal extends Component {
   render() {
     return (
-      <div>export</div>
+      <div>
+        <h2>Export Calendar</h2>
+        <p>
+          Export your Diary data
+        </p>
+        <Button
+          onClick={() => {this.props.deleteAccount()}}
+          primary
+          >
+          Export
+        </Button>
+      </div>
     )
   }
 }
