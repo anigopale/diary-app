3dc4b9b4ce0a144280a33d2fe732080744824a73
Aniruddh Gopale <anigopale@gmail.com>
2018-01-30 11:19:42 +0530

    fix navbar

--- src/components/auth/diary-menu.jsx
+++ src/components/auth/diary-menu.jsx
@@ -9,7 +9,7 @@ class Dmenu extends Component {
 
 
 
-  renderItems() {
+  renderMobileItems() {
 
     return [
       <Link to='/' key={1}>
@@ -19,37 +19,45 @@ class Dmenu extends Component {
         </Menu.Item>
       </Link>,
 
-      <Responsive {...Responsive.onlyMobile}>
-        <Link to='/settings' key={2}>
-          <Menu.Item
-            >
-            <Icon name="settings" />Settings
-          </Menu.Item>
-        </Link>
-      </Responsive>,
-
-        <Menu.Item
-          key={3}
-          position="right"
-          onClick={() => {this.props.logout()}}
-          >
-          <Responsive {...Responsive.onlyMobile}>
-            <Icon name="log out" />Logout
-          </Responsive>
-        </Menu.Item>,
-
         <Menu.Item
           key={3}
           position="right"
           >
-          <Responsive minWidth={Responsive.onlyTablet.minWidth} position="right">
+
             <Dropdown text={localStorage.getItem('user')} icon='user' floating labeled button className='icon'>
               <Dropdown.Menu>
                 <Dropdown.Item icon="settings" text="Settings" onClick={() => {history.push('/settings')}} />
                 <Dropdown.Item onClick={() => {this.props.logout()}} icon="log out" text="Logout" />
               </Dropdown.Menu>
             </Dropdown>
-        </Responsive>
+
+        </Menu.Item>
+    ];
+
+  }
+
+  renderDesktopItems() {
+
+    return [
+      <Link to='/' key={1}>
+        <Menu.Item
+          >
+          <Icon name="home" />Home
+        </Menu.Item>
+      </Link>,
+
+      <Menu.Item
+          key={3}
+          position="right"
+          >
+
+            <Dropdown text={localStorage.getItem('user')} icon='user' floating labeled button className='icon'>
+              <Dropdown.Menu>
+                <Dropdown.Item icon="settings" text="Settings" onClick={() => {history.push('/settings')}} />
+                <Dropdown.Item onClick={() => {this.props.logout()}} icon="log out" text="Logout" />
+              </Dropdown.Menu>
+            </Dropdown>
+
         </Menu.Item>
     ];
 
@@ -58,11 +66,23 @@ class Dmenu extends Component {
 
 
 
+
   render() {
     return (
-      <Menu inverted vertical={this.props.vertical}>
-        {this.renderItems()}
-      </Menu>
+      <div>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <Menu inverted vertical={this.props.vertical}>
+            {this.renderDesktopItems()}
+          </Menu>
+        </Responsive>
+
+        <Responsive {...Responsive.onlyMobile}>
+          <Menu inverted vertical={this.props.vertical}>
+            {this.renderMobileItems()}
+          </Menu>
+        </Responsive>
+
+      </div>
     )
   }
 }
