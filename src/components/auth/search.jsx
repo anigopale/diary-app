f7bd8b3c0ecf4da949ba2e0075d312b75b6927e5
Aniruddh Gopale <anigopale@gmail.com>
2018-02-03 12:20:28 +0530

    search component filters data on search

--- src/components/auth/search.jsx
+++ src/components/auth/search.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Divider } from 'semantic-ui-react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 class Search extends Component {
+  renderResults() {
+    var results=_.filter(this.props.data,(entry) => {
+      return entry.note.indexOf(this.props.search_term)>-1;
+    });
+  }
+
   render() {
     return (
       <div>
         <Link to="/">
           <Button>Back</Button>
         </Link>
+        <h2>Results for '{this.props.search_term}'</h2>
+        <Divider />
+        {this.renderResults()}
       </div>
     )
   }
 }
 
-export default connect(null)(Search);
+function mapStateToProps({ search_term, data }) {
+  return { search_term, data };
+}
+
+export default connect(mapStateToProps)(Search);
